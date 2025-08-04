1a4ba5fa5141dd20b5e2878bdc7ed7776b321c5f
Chirag Pandya <136418696+ChiragPandya31@users.noreply.github.com>
2025-08-04 12:21:53 +0530

    Update pyqController.js

--- controllers/pyqController.js
+++ controllers/pyqController.js
@@ -1,21 +1,23 @@
+import fs from "fs/promises";
 import analyzePYQ from "../utils/analyzePYQ.js";
 
 export const analyzePYQController = async (req, res) => {
-  if (!req.files || !req.files.pdfs) {
-    return res.status(400).json({ error: "No PDF files uploaded." });
-  }
-
-  const pdfFiles = Array.isArray(req.files.pdfs)
-    ? req.files.pdfs
-    : [req.files.pdfs];
-
-  const buffers = await Promise.all(
-    pdfFiles.map(async (file) => {
-      return { buffer: file.data, filename: file.name };
-    })
-  );
-
   try {
+    if (!req.files || !req.files.pdfs) {
+      return res.status(400).json({ error: "No PDF files uploaded." });
+    }
+
+    const pdfFiles = Array.isArray(req.files.pdfs)
+      ? req.files.pdfs
+      : [req.files.pdfs];
+
+    const buffers = await Promise.all(
+      pdfFiles.map(async (file) => {
+        const buffer = await fs.readFile(file.tempFilePath);
+        return { buffer, filename: file.name };
+      })
+    );
+
     const result = await analyzePYQ(buffers);
     res.json(result);
   } catch (err) {
